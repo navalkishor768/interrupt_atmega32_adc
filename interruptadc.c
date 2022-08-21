1ffbb243bc62059c70c4ddd642cef2589b9c8091
Navalkishor <navalkishor768@gmail.com>
2022-08-21 09:52:20 +0530

    unused library removed and indentation in interruptadc.c

--- interruptadc.c
+++ interruptadc.c
@@ -1,26 +1,21 @@
 #include<avr/io.h>
 #include<util/delay.h>
-//#include<compat/deprecated.h>
-
 #include<avr/interrupt.h>
 #include"uartnaval.h"
 #include"uartnaval.c"
-//#include"lcdgdheerajat8.h"
-//#include"lcdgdheerajat8.c"
-//#include"adcatmega16.h"
-//#include"adcatmega16.c"
 
 
-#define Nbitdiv  512.0  // 512 for 10 bit adc, 128 for 8 bit adc
-#define factor   (5/(Nbitdiv*2))
-//#define mVperA	0.2		// for ACS712 5A module
-#define mVperA	0.1		// for ACS712 20A module
-//#define mVperA	0.066		// for ACS712 30A module
-#define factor2		(5/Nbitdiv*2)
-#define voltagemeasure		24
-#define fullscale			4.8
+
+#define Nbitdiv  		512.0  // 512 for 10 bit adc, 128 for 8 bit adc
+#define factor   		(5/(Nbitdiv*2))
+//#define mVperA		0.2		// for ACS712 5A module
+#define mVperA			0.1		// for ACS712 20A module
+//#define mVperA		0.066		// for ACS712 30A module
+#define factor2			(5/Nbitdiv*2)
+#define voltagemeasure	24
+#define fullscale		4.8
 #define batvscale		(voltagemeasure/fullscale)
-#define dcwatthr	(v*btv)
+#define dcwatthr		(v*btv)
 
 
 unsigned char str[20],st[1],str2[20];
@@ -48,15 +43,8 @@ int main()
 	_delay_ms(20);
 	while(1) 
 	{	
-	
-		/*sprintf(str,"ADCH is %d",j);
-		uart_txstr(str);
-		_delay_ms(500);
-		uart_tx('\r');
-		sprintf(str2,"Vraw is %d",k);
-		uart_txstr(str2);*/
 		
-		z=j - Nbitdiv;		// 
+		z=j - Nbitdiv;		
 		cout=factor * z / mVperA;
 		dtostrf(cout,2,2,str2);
 		
